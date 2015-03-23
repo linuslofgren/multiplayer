dd4035d699d90f71a80a75b3086688cac0ea7043
linuslofgren <pnl.lofgren@gmail.com>
2015-03-23 17:20:55 +0100

    A loop(not working)

--- websockets/main.py
+++ websockets/main.py
@@ -2,6 +2,7 @@ __author__ = 'Linus'
 
 from flask import Flask, render_template, request, jsonify
 from players import add, players
+import threading
 
 app = Flask("__name__")
 
@@ -23,6 +24,23 @@ def calc():
 def add_player():
     add()
     return jsonify(result=players[-1].serialize())
+
+
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
+
+
+def move():
+    for i in players:
+        i.yPos += 1
+
+
+set_interval(move, 10)
 
 if __name__ == "__main__":
     app.debug = True
